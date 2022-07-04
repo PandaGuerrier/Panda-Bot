c726157fcae68e327f272ee8c5910728a6e43cb1
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2022-07-04 11:55:01 +0200

    Update setup.js

--- slashCommands/setup.js
+++ slashCommands/setup.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js")
+const { MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu } = require("discord.js")
 const config = require("../config/config.json")
 
 module.exports = {

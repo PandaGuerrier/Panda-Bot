ee669c2b29f28f20eec5d7935c0b183b0391d04f
PandaGuerrier <juleslofficial@gmail.com>
2022-08-26 19:03:51 +0200

    $:l

--- events/ready.js
+++ events/ready.js
@@ -18,8 +18,8 @@ module.exports = {
     client.user.setActivity(`${config.informations.status}`, {
       type: "PLAYING",
     })
-    setInterval(() => {
-      await guild.channels.cache.get(config.channels.stats).setName(`📝・${member.guild.memberCount} membres`)
+    setInterval(async () => {
+      await guild.channels.cache.get(config.channels.stats).setName(`📝・${guild.memberCount} membres`)
     }, 15000);
   }
 }
--- utils/client.js
+++ utils/client.js
@@ -34,15 +34,3 @@ class PandaClient {
 }
 
 module.exports = PandaClient
-
-
-
-
-/*
-
-Pour mieux réexpliquer : 
-- Une meilleure utilisation de la bdd : que ce soit pour la conception : avec les tables du giveaway par exemple ; ou pour l'utilisation : un orm ce sera toujours mieux que de faire ses requêtes comme tu les fait là
-- Bonne maîtrise du langage : tout ce que je t'avais déjà dit sur les mélanges de async/await fonctions callback et .then; la structure du code qui devient de plus en plus illisible au fur et a mesure que tu rajoute des éléments (ex : boutonInteraction.js) + c'est mélangé, ce n'est pas les commentaires qui feront que ton code sera mieux structuré
-réduire un maximum ton nombre d'indentation (la plupart sont inutiles) selon moi ça nuit grandement a la lisibilité surtout sur des gros fichiers
-
-*/
